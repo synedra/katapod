da231bef6dbdbff393d864eccd78c7810856f5ef
Aleksandr Volochnev <a.volochnev@gmail.com>
2022-04-07 16:38:53 +0200

    Links support, commands support

--- extension.js
+++ extension.js
@@ -7,6 +7,7 @@ const MarkdownIt = require('markdown-it');
 module.exports = {activate, deactivate}
 
 terminal = null
+panel = null
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -14,22 +15,39 @@ terminal = null
 function activate(context) {
 	inform('Katapod is enabled')
 
-    // console.log(vscode.workspace.workspaceFolders[0].uri.path)
+// console.log(renderCommandUri('cqlsh'))
+// console.log(renderCommandUri('describe keyspaces;'))
 
-	const panel = vscode.window.createWebviewPanel(
+	panel = createPanel()
+	loadPage({step: 'step1'})
+
+	terminal = vscode.window.createTerminal('cqlsh')
+	terminal.show()
+	
+	context.subscriptions.push(vscode.commands.registerCommand('katapod.sendText', sendText));
+
+	context.subscriptions.push(vscode.commands.registerCommand('katapod.loadPage', loadPage));
+}
+
+function createPanel () {
+	return vscode.window.createWebviewPanel(
 		'catWebview',
 		'Cat Webview',
 		vscode.ViewColumn.One,
 		{
 			enableCommandUris: true,
 			enableScripts: true,
-			retainContextWhenHidden: true
+			retainContextWhenHidden: true,
+			enableFindWidget: true
 		}
 	)
+}
 
-	const file = vscode.Uri.file(path.join(vscode.workspace.workspaceFolders[0].uri.path, 'step1.md'));
+function loadPage (target) {
 
-	const md = new MarkdownIt().use(require('markdown-it-textual-uml'));;
+	const file = vscode.Uri.file(path.join(vscode.workspace.workspaceFolders[0].uri.path, target.step + '.md'));
+
+	const md = new MarkdownIt({html: true}).use(require('markdown-it-textual-uml'));;
 
 	const pre = `<!DOCTYPE html>
 	<html lang="en">
@@ -38,19 +56,31 @@ function activate(context) {
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<script src="https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js"></script>
 		<script>mermaid.initialize({startOnLoad:true});</script>
+		<style>
+			pre code {background-color: lightgray; color: black; margin: 0 10px 0 10px; padding: 10px 10px; width: 100%; display: block;}
+		</style>
 	</head>
 	<body>`
 	const post = `</body></html>`
 	var result = md.render((fs.readFileSync(file.fsPath, 'utf8')))
 
 	panel.webview.html = pre + result + post
+}
 
-	terminal = vscode.window.createTerminal('cqlsh')
+function sendText (arguments) {
+	terminal.sendText(arguments.command)
+}
 
-	context.subscriptions.push(vscode.commands.registerCommand('katapod.sendText', function () {
-		terminal.sendText('cqlsh')
-		// console.log('sendText')
-	}));
+function renderStepUri (step) {
+	const uri = encodeURIComponent(JSON.stringify([{ 'step': step }])).toString();
+	console.log(uri)
+	return uri
+}
+
+function renderCommandUri (command) {
+	const uri = encodeURIComponent(JSON.stringify([{ 'command': command }])).toString();
+	console.log(uri)
+	return uri
 }
 
 function inform (message) {
