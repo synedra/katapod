b2d0d98e77a6d981e50f7c921387c0c726180853
Aleksandr Volochnev <a.volochnev@gmail.com>
2022-05-29 00:01:41 +0200

    Improved wait for c*

--- extension.js
+++ extension.js
@@ -23,7 +23,7 @@ function activate(context) {
 
 	terminal = vscode.window.createTerminal('cqlsh')
 	terminal.show()
-	terminal.sendText("echo -n 'Waiting for Cassandra to start...'; timeout 60 bash -c 'until printf \"\" 2>>/dev/null >>/dev/tcp/localhost/9042; do sleep 2; echo -n \".\"; done'; echo ' Ready!'")
+	terminal.sendText("echo -n 'Waiting for Cassandra to start...'; timeout 60 bash -c 'until cqlsh -e \"describe cluster\" >/dev/null 2>&1; do sleep 2; echo -n \".\"; done'; echo ' Ready!'")
 
 	context.subscriptions.push(vscode.commands.registerCommand('katapod.sendText', sendText));
 	context.subscriptions.push(vscode.commands.registerCommand('katapod.loadPage', loadPage));
